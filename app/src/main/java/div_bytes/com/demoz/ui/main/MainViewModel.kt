243dbd63e6d98735d2fde332961117e0b0c40ce5
div <divij@hello.com>
2018-06-07 00:00:07 -0700

    removing un-needed imports from MainViewModel

--- app/src/main/java/div_bytes/com/demoz/ui/main/MainViewModel.kt
+++ app/src/main/java/div_bytes/com/demoz/ui/main/MainViewModel.kt
@@ -3,11 +3,7 @@ package div_bytes.com.demoz.ui.main
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.ViewModel
-import android.content.res.AssetManager
-import div_bytes.com.demoz.R
 import div_bytes.com.demoz.repo.VehicleRepository
-import java.io.FileInputStream
 
 class MainViewModel(application : Application) : AndroidViewModel(application) {
     private lateinit var vehicleRepo : VehicleRepository
