c6236111b6c15ddcd64761df1887ace4be76dd1d
div <divij@hello.com>
2018-06-07 00:00:57 -0700

    finished the adapter and observing code for livedata in mainFragment

--- app/src/main/java/div_bytes/com/demoz/ui/main/MainFragment.kt
+++ app/src/main/java/div_bytes/com/demoz/ui/main/MainFragment.kt
@@ -1,18 +1,23 @@
 package div_bytes.com.demoz.ui.main
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.content.Context
 import android.os.Bundle
 import android.support.annotation.NonNull
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import div_bytes.com.demoz.R
-import org.jetbrains.annotations.NotNull
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.main_fragment.*
+import org.jetbrains.annotations.NotNull
 
 
 data class VehiclesJSON(val vehicles: List<Vehicle>)
@@ -23,6 +28,8 @@ data class Vehicle(@PrimaryKey @NonNull val id: String,
 
 class MainFragment : Fragment() {
 
+    lateinit var viewModel: MainViewModel
+    lateinit var adapter: VehicleListAdapter
 
     companion object {
         fun newInstance() = MainFragment()
@@ -47,13 +54,53 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = VehicleListAdapter(this)
-        vehiclesListRecyclerView.setAdapter(adapter)
-        vehiclesListRecyclerView.setLayoutManager(LinearLayoutManager(this))
+        viewModel = ViewModelProviders.of(activity as FragmentActivity).get(MainViewModel::class.java)
 
+        vehicleListAdapter()
+
+        viewModel.vehiclesLiveData.observe(this, Observer<List<Vehicle>> { adapter.setData(it!!) })
+
+        vehiclesListRecyclerView.setAdapter(adapter)
+        vehiclesListRecyclerView.setLayoutManager(LinearLayoutManager(activity))
+    }
+
+    private fun vehicleListAdapter() {
+        adapter = VehicleListAdapter(activity as Context)
+    }
+}
+
+class VehicleListAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var dataList:List<Vehicle> = ArrayList()
+
+    private lateinit var inflater: LayoutInflater
+
+    constructor(context: Context):this() {
+        inflater = LayoutInflater.from(context)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return object: RecyclerView.ViewHolder(inflater.inflate(R.layout.vehicle_list_item, parent, false)) {
+
+            var nameTextView: TextView = itemView.findViewById(R.id.vehicleNameTextView)
+
+            fun bindData(vehicle: Vehicle) {
+                nameTextView.text = vehicle.name
+            }
+
+        }
 
     }
 
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
 
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
+    }
+
+    fun setData(dataList: List<Vehicle>) {
+        this.dataList = dataList
+        notifyDataSetChanged()
+    }
 }
